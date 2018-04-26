870f50ff775ae4671af65a40121265e8f27ed3c0
Omar Rasheed <omarrasheed@omars-imac.local>
2018-04-26 14:08:55 -0700

    Just added pods n initialized the view

--- 4YearPlanner/ViewController.swift
+++ 4YearPlanner/ViewController.swift
@@ -7,30 +7,36 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
-    var myCollege: College
-    var myMajor: Major
-    var myMinor: Requirements
-    var allClasses: [Class]
-    var allSemesters: [Semester]
+//    var myCollege: College
+//    var myMajor: Major
+//    var myMinor: Requirements
+    var allClasses: [Class] = []
+    var allSemesters: [Semester] = []
+    
+    var homeStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         view.backgroundColor = .white
         
+        homeStackView = UIStackView()
+        
+        
     }
     /** Return: list of all college/major/minor requirements that are left unsatisfied,
      empty list if all requirements are satisfied */
-    func requirementsLeft() -> [Class] {
-        var requirementsLeft = [Class]()
-        requirementsLeft+=myCollege.requirementsLeft(classes: allClasses)
-        requirementsLeft+=myMajor.requirementsLeft(classes: allClasses)
-        requirementsLeft+=myMinor.requirementsLeft(classes: allClasses)
-        return requirementsLeft
-    }
+//    func requirementsLeft() -> [Class] {
+//        var requirementsLeft = [Class]()
+//        requirementsLeft+=myCollege.requirementsLeft(classes: allClasses)
+//        requirementsLeft+=myMajor.requirementsLeft(classes: allClasses)
+//        requirementsLeft+=myMinor.requirementsLeft(classes: allClasses)
+//        return requirementsLeft
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ Pods/Target Support Files/Pods-4YearPlanner/Pods-4YearPlanner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_4YearPlanner : NSObject
+@end
+@implementation PodsDummy_Pods_4YearPlanner
+@end
